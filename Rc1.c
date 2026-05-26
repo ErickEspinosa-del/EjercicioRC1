7b13e4da8e9d4e0bf28a389d393ce7b6ace61e02
Harol Rosero <harol.rosero@udla.edu.ec>
2026-05-26 14:31:23 -0500

    segundo commit

--- Rc1.c
+++ Rc1.c
@@ -3,5 +3,12 @@
 int main (int argc, char *argv[]) {
     int a;
     int b;
+    int suma;
+    printf ("ingrese el valor de a");
+    scanf ("%d", &a);
+    printf("ingrese el valor de b");
+    scanf("%d", &b);
+    suma=a+b;
+    printf("la suma es:");
     return 0;
 }
