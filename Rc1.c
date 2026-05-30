818b2042e906d60dfb478400aa28b14630ce1143
Harol Rosero <harol.rosero@udla.edu.ec>
2026-05-30 11:01:24 -0500

    Segundo commit, ingreso de logicas para promedios, maximos y minimos

--- Rc1.c
+++ Rc1.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
     void procesarmatriz(int numEstudiantes, float notas[numEstudiantes][3], float reporte[3][3]){
+        float suma;
+
+        for (int col = 0; col < 3; col++)
+        {
+            float suma=0;
+            float max= notas [0][col];
+            float min= notas [0][col];
+            for (int fila = 0; fila < numEstudiantes; fila++)
+            {
+                suma=suma + notas[fila][col];
+                if (notas[fila][col]>max)
+                {
+                    max=notas[fila][col];
+                }
+                if (notas[fila][col]<min)
+                {
+                    min=notas[fila][col];
+                }
+                
+            }
+            
+        }
         
+        
+
     }
     
